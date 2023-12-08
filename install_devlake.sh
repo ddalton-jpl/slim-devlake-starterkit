bd0d0901aeb23105acfb9ec604aebe06c18e7de4
ddalton <dillon.s.dalton@jpl.nasa.gov>
2023-12-08 08:07:56 -0800

    refactor: improving install script

--- install_devlake.sh
+++ install_devlake.sh
@@ -9,12 +9,15 @@ echo "Step 1: Prerequisites"
 echo "Make sure you have Docker v19.03.10+ and docker-compose v2.2.3+ installed."
 echo "If you have Docker Desktop installed, docker-compose is already included."
 
+DOCKER_COMPOSE_URL="https://github.com/apache/incubator-devlake/releases/download/v0.20.0-beta4/docker-compose.yml"
+ENV_EXAMPLE_URL="https://github.com/apache/incubator-devlake/releases/download/v0.20.0-beta4/env.example"
+
 # Check if docker-compose.yml and env.example already exist
 if [ ! -e docker-compose.yml ] && [ ! -e env.example ]; then
     # Step 2: Launch DevLake
     echo "Step 2. Downloading docker-compose.yml and env.example"
-    wget https://example.com/path/to/latest/release/docker-compose.yml
-    wget https://example.com/path/to/latest/release/env.example
+    wget $DOCKER_COMPOSE_URL
+    wget $ENV_EXAMPLE_URL
 
     echo "Step 3. Renaming env.example to .env..."
     mv env.example .env
@@ -27,7 +30,17 @@ encryption_key=$(openssl rand -base64 2000 | tr -dc 'A-Z' | fold -w 128 | head -
 
 # Method 2: Alternatively, you can set the ENCRYPTION_SECRET environment variable in the .env file
 echo "Method 2: Setting in .env file"
-echo "ENCRYPTION_SECRET=\"$encryption_key\"" >>.env
+
+# Check if .env file exists
+if [ -e .env ]; then
+    # Read existing content
+    existing_content=$(cat .env)
+    # Append new content to existing content
+    printf "%s\nENCRYPTION_SECRET=\"%s\"\n" "$existing_content" "$encryption_key" >.env
+else
+    # If .env doesn't exist, create it with the new content
+    printf "ENCRYPTION_SECRET=\"%s\"\n" "$encryption_key" >.env
+fi
 
 # Step 5: Run Docker Compose to launch DevLake
 echo "Step 5: Launching DevLake with Docker Compose"
